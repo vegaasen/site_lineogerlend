0271d0ab1681a3a1280466590d62ad96020d7c83
Vegard Aasen <vegard.aasen@webstep.no>
2018-11-26 21:19:35 +0100

    Included GA

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-129862709-1');
 
 ReactDOM.render(<App />, document.getElementById('root'));
